814b9b9e575bacbd789f0d1cc393036f310f5b45
Helon Bentes Bastos Xavier <71794862+Helon21@users.noreply.github.com>
2024-03-21 10:31:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import random
 from random import shuffle
 
-quantidade_participantes = int(input("Informe a quantidade de participantes"))
+quantidade_participantes = int(input("Informe a quantidade de participantes: \n"))
 nomes = []
 
 for i in range(quantidade_participantes):
-    inserirNome = input("Digite o nome do participante")
+    inserirNome = input("Digite o nome do participante: \n")
     nomes.append(inserirNome)
 
 shuffle(nomes)
